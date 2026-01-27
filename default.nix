8b87abc27d2690e7052bd11b0960f420c18eead3
Andrew Torgesen <andrew.torgesen@gmail.com>
2026-01-26 20:21:45 -0800

    Add more wallpapers with a generator (#7)

--- default.nix
+++ default.nix
@@ -1,8 +1,10 @@
-{ pkgs }:
+{ pkgs ? import (fetchTarball "https://github.com/goromal/anixpkgs/archive/refs/heads/master.tar.gz") {} }:
 let 
     mkData = name: data: {
         inherit name data;
     };
+    minWallpaperIdx = 1;
+    maxWallpaperIdx = 18;
 in with pkgs; rec {
     apps = {
         la-quiz = {
@@ -16,20 +18,15 @@ in with pkgs; rec {
         nexa = mkData "nexa.ttf" ./data/fonts/nexa.ttf;
     };
     img = {
-        wallpapers = {
-            wallpaper1 = mkData "wallpaper.png" ./data/img/wallpaper1.png;
-            wallpaper2 = mkData "wallpaper.png" ./data/img/wallpaper2.png;
-            wallpaper3 = mkData "wallpaper.png" ./data/img/wallpaper3.png;
-            wallpaper4 = mkData "wallpaper.png" ./data/img/wallpaper4.png;
-            wallpaper5 = mkData "wallpaper.png" ./data/img/wallpaper5.png;
-            wallpaper6 = mkData "wallpaper.png" ./data/img/wallpaper6.png;
-            wallpaper7 = mkData "wallpaper.png" ./data/img/wallpaper7.png;
-            wallpaper8 = mkData "wallpaper.png" ./data/img/wallpaper8.png;
-            wallpaper9 = mkData "wallpaper.png" ./data/img/wallpaper9.png;
-            wallpaper10 = mkData "wallpaper.png" ./data/img/wallpaper10.png;
-            wallpaper11 = mkData "wallpaper.png" ./data/img/wallpaper11.png;
-            wallpaper12 = mkData "wallpaper.png" ./data/img/wallpaper12.png;
-        };
+        wallpapers = (builtins.listToAttrs (
+            builtins.genList (i:
+                let idx = i + minWallpaperIdx;
+                in {
+                    name = "wallpaper${toString idx}";
+                    value = mkData "wallpaper.png" ./data/img/wallpaper${toString idx}.png;
+                }
+            ) (maxWallpaperIdx - minWallpaperIdx + 1)
+        )) // { inherit minWallpaperIdx maxWallpaperIdx; };
         ajt-logo-white = mkData "ajt.png" ./data/img/ajt.png;
         anix-logo-bmp  = mkData "anixpkgs.png" ./data/img/anixpkgs.png;
         anix-logo-svg  = mkData "anixpkgs.svg" ./data/img/anixpkgs.svg;
